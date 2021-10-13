04f0591647ec82d2c20ff3262df290573e4e7a3b
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-10-13 15:31:49 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Canvas, extend } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro";
+import "./App.css";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const WaveShaderMaterial = shaderMaterial(
+  // uniform
+  {},
+  // vertex shader
+  glsl`
+  void main(){
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+  }
+  
+  `,
+  // Fragment shader
+  glsl`
+  void main(){
+    gl_FragColor = vec4(0.0,0.4,1.0,1.0)
+  }
+  `
+);
+
+extend({
+  WaveShaderMaterial,
 });
+
+const Scene = () => {
+  return (
+    <Canvas>
+      <pointLight position={[10, 10, 10]} />
+      <mesh>
+        <planeBufferGeometry args={[3, 5]} />
+        <waveShaderMaterial />
+      </mesh>
+    </Canvas>
+  );
+};
+
+const App = () => {
+  return <Scene />;
+};
+
+export default App;
